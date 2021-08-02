00a674900645480f7fb97fdeb706f3037fafb351
rendra <rendrasuwondo@gmail.com>
2021-08-02 21:17:20 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -11,6 +11,10 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, temporibus!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 
 </html>
